efd22063174ee715a84f86dbbefc54c4d6e1fa80
EgorK2606 <kramere26@gmail.com>
2020-08-06 09:59:04 +0200

    First commit with empty project

---
+++ BMIcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMIcalculator
+//
+//  Created by Gregor Kramer on 06.08.2020.
+//  Copyright © 2020 Gregor Kramer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
